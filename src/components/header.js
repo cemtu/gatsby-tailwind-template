589a113bbbd35e6157b7ddf244e1f4c32805d829
Cem <cem_tuncelli@protonmail.ch>
2021-04-29 23:08:08 +0300

    initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,10 @@
-import * as React from "react"
-import PropTypes from "prop-types"
-import { Link } from "gatsby"
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <header>
+    <div>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
